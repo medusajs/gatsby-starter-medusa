cb94890236a0d7b871462bfa1ff030896bde9b75
Kasper <kasper@medusajs.com>
2022-02-15 09:37:05 +0100

    fix: removed ability to change product quantity while in checkout to prevent no shipping

--- src/components/checkout/checkout-item.jsx
+++ src/components/checkout/checkout-item.jsx
@@ -1,13 +1,8 @@
 import React from "react"
-import { useCart } from "../../hooks/use-cart"
 import { formatPrice } from "../../utils/format-price"
-import QuantitySelector from "../products/quantity-selector"
 import ImageContainer from "../utility/image-container"
 
 const CheckoutItem = ({ item, currencyCode }) => {
-  const {
-    actions: { removeItem, updateQuantity },
-  } = useCart()
   return (
     <div className="flex">
       <div className="mr-4">
@@ -19,21 +14,19 @@ const CheckoutItem = ({ item, currencyCode }) => {
             <p className="font-semibold mb-2">{item.title}</p>
             <p>Variant: {item.description}</p>
           </div>
-          <p>{formatPrice(item.unit_price, currencyCode, item.quantity)}</p>
-        </div>
-        <div className="flex flex-col justify-between pr-2">
-          <div className="flex justify-end">
-            <button onClick={async () => removeItem(item.id)}>&times;</button>
+          <div>
+            <p className="mb-2">Quantity: {item.quantity}</p>
+            <p className="font-medium">
+              Total: {formatPrice(item.unit_price, currencyCode, item.quantity)}
+            </p>
           </div>
-          <QuantitySelector
-            quantity={item.quantity}
-            increment={async () =>
-              updateQuantity({ id: item.id, quantity: item.quantity + 1 })
-            }
-            decrement={async () =>
-              updateQuantity({ id: item.id, quantity: item.quantity - 1 })
-            }
-          />
+        </div>
+        <div>
+          <p>
+            <p className="font-medium">
+              {formatPrice(item.unit_price, currencyCode, 1)}
+            </p>
+          </p>
         </div>
       </div>
     </div>
